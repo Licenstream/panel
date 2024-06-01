44d0139e1582dda621117fc9268d9dbc7f46a7bf
Thijs Naus <124517822+tnausfontys@users.noreply.github.com>
2024-06-01 23:21:56 +0200

    extr afiles

---
+++ InfrastructureEF/LicenseModels/LicenseServiceStatus.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InfrastructureEF.LicenseModels;
+
+public partial class LicenseServiceStatus
+{
+    public int? LicenseId { get; set; }
+    public int? ServiceStatusId { get; set; }
+}
---
+++ InfrastructureEF/LicenseModels/ProductNames.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InfrastructureEF.LicenseModels;
+
+public partial class ProductNames
+{
+    public string? ProductDisplayName { get; set; }
+
+    public string? StringId { get; set; }
+
+    public string? Guid { get; set; }
+
+    public string? ServicePlanName { get; set; }
+
+    public string? ServicePlanId { get; set; }
+
+    public string? ServicePlansIncludedFriendlyNames { get; set; }
+}
